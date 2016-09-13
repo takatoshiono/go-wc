ac41270dadfe6fff1e521ce3f842dae59ac099ec
Takatoshi Ono <takatoshi.ono@gmail.com>
2016-09-13 13:09:36 +0900

    Go format using vim-go

--- go-wc.go
+++ go-wc.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "bytes"
-    "os"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type WordCount struct {
-    filename string
-    lineCount int
-    bytes int
-    wordCount int
+	filename  string
+	lineCount int
+	bytes     int
+	wordCount int
 }
 
 func (wc *WordCount) CountLines() error {
-    b, err := ioutil.ReadFile(wc.filename)
-    if err != nil {
-        return err
-    }
-    wc.lineCount = bytes.Count(b, []byte{'\n'})
-    return nil
+	b, err := ioutil.ReadFile(wc.filename)
+	if err != nil {
+		return err
+	}
+	wc.lineCount = bytes.Count(b, []byte{'\n'})
+	return nil
 }
 
 func (wc *WordCount) CountBytes() error {
-    b, err := ioutil.ReadFile(wc.filename)
-    if err != nil {
-        return err
-    }
-    wc.bytes = len(b)
-    return nil
+	b, err := ioutil.ReadFile(wc.filename)
+	if err != nil {
+		return err
+	}
+	wc.bytes = len(b)
+	return nil
 }
 
 func (wc *WordCount) CountWords() error {
-    b, err := ioutil.ReadFile(wc.filename)
-    if err != nil {
-        return err
-    }
-    wc.wordCount = len(bytes.Fields(b))
-    return nil
+	b, err := ioutil.ReadFile(wc.filename)
+	if err != nil {
+		return err
+	}
+	wc.wordCount = len(bytes.Fields(b))
+	return nil
 }
 
 func (wc *WordCount) CountAll() error {
-    var err error
+	var err error
 
-    err = wc.CountLines()
-    if err != nil {
-        return err
-    }
+	err = wc.CountLines()
+	if err != nil {
+		return err
+	}
 
-    err = wc.CountBytes()
-    if err != nil {
-        return err
-    }
+	err = wc.CountBytes()
+	if err != nil {
+		return err
+	}
 
-    err = wc.CountWords()
-    if err != nil {
-        return err
-    }
+	err = wc.CountWords()
+	if err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func (wc *WordCount) Show() {
-    fmt.Printf("%d\t%d\t%d\t%s\n", wc.lineCount, wc.wordCount, wc.bytes, wc.filename)
+	fmt.Printf("%d\t%d\t%d\t%s\n", wc.lineCount, wc.wordCount, wc.bytes, wc.filename)
 }
 
 func main() {
-    for _, filename := range os.Args[1:] {
-        wc := WordCount{filename, 0, 0, 0}
-        err := wc.CountAll()
-        if err != nil {
-            fmt.Println(err)
-            continue
-        }
-        wc.Show()
-    }
+	for _, filename := range os.Args[1:] {
+		wc := WordCount{filename, 0, 0, 0}
+		err := wc.CountAll()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		wc.Show()
+	}
 }
